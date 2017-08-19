b0aa6381fabd77e58b404b680377d2ac11e6ab2a
Adi <adi.bolb@gmail.com>
2017-08-19 11:10:33 +0300

    Added second test with board two by two and x wins on left column

--- 01-intro/src/TicTacToeTests.java
+++ 01-intro/src/TicTacToeTests.java
@@ -23,20 +23,20 @@ public class TicTacToeTests {
     *       * On line
     *       * On diagonal
     *
-    * Guideline: Behavior Slicing
+    * Guideline 2: Behavior Slicing
     * Inputs                Outputs
     * X finished line       X won
     * X finished column     X won
     * X finished diagonal   X won
     *
     *
-    * Guideline: SIMPLIFY!
+    * Guideline 3: SIMPLIFY!
     *
-    * Guideline: Introduce only one notion (domain concept) at a time, one per test
+    * Guideline 4: Introduce only one notion (domain concept) at a time, one per test
     *
-    * Guideline: The rule of three "only extract duplication when spotted at least three times"
+    * Guideline 5: The rule of three "only extract duplication when spotted at least three times"
     *
-    * Guideline: Triangulation
+    * Guideline 6: Triangulation
     *
     * */
 
@@ -48,6 +48,26 @@ public class TicTacToeTests {
 
         // Production code
         String gameResult = board == "one by one" ? "X won" : "Nobody won";
+
+        // Act
+        String actual = gameResult;
+
+        // Assert
+        assertEquals(expected, actual);
+    }
+
+    /*
+    * X 0
+    * X
+    * */
+    @Test
+    public void forTwoByTwoBoardXWinsOnColumn(){
+        // Arrange
+        String board = "two by two with X on left column";
+        String expected = "X won";
+
+        // Production code
+        String gameResult = board == "two by two with X on left column" ? "X won" : "Nobody won";
 
         // Act
         String actual = gameResult;
