d850388d39f52c285f07ec5176b7a02f01d9ce87
Adi <adi.bolb@gmail.com>
2017-09-10 18:24:57 +0300

    Added tests for equality on Board

--- 01-intro/src/BoardTests.java
+++ 01-intro/src/BoardTests.java
@@ -4,6 +4,9 @@ import tictactoe.Board;
 import tictactoe.BoardBuilder;
 
 import static junit.framework.TestCase.assertEquals;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class BoardTests {
 
@@ -42,5 +45,43 @@ public class BoardTests {
         String actual = board.getTemplate();
 
         assertEquals(boardTemplate, actual);
+    }
+
+    @Test
+    public void boardInstanceIsEqualToItself(){
+        Board board = boardBuilder.withStructure("good")
+                .withSize("good as well")
+                .withTemplate("very good")
+                .build();
+
+        assertEquals(board, board);
+    }
+
+    @Test
+    public void twoBoardInstancesWithSameValuesAreEqual(){
+        Board firstBoard = boardBuilder.withStructure("good")
+                .withSize("good size")
+                .withTemplate("good template")
+                .build();
+        Board secondBoard = boardBuilder.withStructure("good")
+                .withSize("good size")
+                .withTemplate("good template")
+                .build();
+
+        assertEquals(firstBoard, secondBoard);
+    }
+
+    @Test
+    public void twoBoardInstancesWithDifferentValuesAreNotEqual(){
+        Board firstBoard = boardBuilder.withStructure("good 1")
+                .withSize("good size 1")
+                .withTemplate("good template 1")
+                .build();
+        Board secondBoard = boardBuilder.withStructure("good 2")
+                .withSize("good size 2")
+                .withTemplate("good template 2")
+                .build();
+
+        assertThat(firstBoard, is(not(secondBoard)));
     }
 }
--- 01-intro/src/tictactoe/Board.java
+++ 01-intro/src/tictactoe/Board.java
@@ -22,4 +22,24 @@ public class Board {
     public String getTemplate() {
         return template;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Board board = (Board) o;
+
+        if (template != null ? !template.equals(board.template) : board.template != null) return false;
+        if (structure != null ? !structure.equals(board.structure) : board.structure != null) return false;
+        return size != null ? size.equals(board.size) : board.size == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = template != null ? template.hashCode() : 0;
+        result = 31 * result + (structure != null ? structure.hashCode() : 0);
+        result = 31 * result + (size != null ? size.hashCode() : 0);
+        return result;
+    }
 }
